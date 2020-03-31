734d9991833ea0a76f37eed46ea5bd4f8148e3c8
terminal29 <20899505+terminal29@users.noreply.github.com>
2020-03-31 17:03:41 +1000

    Some bugfixes, added some config and commands

--- src/main/java/com/terminal29/DiscordBot.java
+++ src/main/java/com/terminal29/DiscordBot.java
@@ -2,35 +2,32 @@ package com.terminal29;
 
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
-import net.dv8tion.jda.api.entities.Activity;
-import net.dv8tion.jda.api.entities.GuildChannel;
-import net.dv8tion.jda.api.entities.MessageChannel;
-import net.dv8tion.jda.api.entities.TextChannel;
+import net.dv8tion.jda.api.entities.*;
 import net.dv8tion.jda.api.events.GenericEvent;
 import net.dv8tion.jda.api.events.ReadyEvent;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 
 import javax.annotation.Nonnull;
 import javax.security.auth.login.LoginException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class DiscordBot implements EventListener {
     static JDA _bot = null;
     static String _token = null;
     static long _channelID = -1;
     static boolean _botReady = false;
+    static boolean _showStatusMessages = false;
 
-    static void setToken(String token){
-        _token = token;
-    }
-
-    static void setChannel(long channel){
-        _channelID = channel;
-    }
-
-    static void initBot() throws Exception {
+    static void initBot(String token, long channelID, boolean showStatusMessages) throws Exception {
         if(_bot == null){
+            _token = token;
+            _channelID = channelID;
+            _showStatusMessages = showStatusMessages;
+
             JDABuilder builder = new JDABuilder(_token);
             builder.setActivity(Activity.playing("Minecraft"));
             builder.addEventListeners(new DiscordBot());
@@ -45,17 +42,23 @@ public class DiscordBot implements EventListener {
     static void cleanupBot(){
         if(_bot != null){
             TextChannel channel = _bot.getTextChannelById(_channelID);
-            channel.sendMessage("Bot shutting down").queue();
+            if(channel != null && _showStatusMessages)
+                channel.sendMessage("MCord going offline o/").queue();
+            _bot.shutdown();
+            _bot = null;
         }
     }
 
     static void onMCUserMessage(String user, String message){
-        TextChannel channel = _bot.getTextChannelById(_channelID);
-        channel.sendMessage(String.format("[%s] %s", user, message)).queue();
+        if(_bot != null) {
+            TextChannel channel = _bot.getTextChannelById(_channelID);
+            if (channel != null)
+                channel.sendMessage(String.format("> **%s:** %s", user, message)).queue();
+        }
     }
 
     static void onDiscordMessage(String user, String message){
-        Bukkit.broadcastMessage(String.format("[Discord: %s]: %s", user, message));
+        Bukkit.broadcastMessage(String.format("\u00A79<%s>\u00A7f %s", user, message));
     }
 
     @Override
@@ -63,13 +66,37 @@ public class DiscordBot implements EventListener {
         if (event instanceof ReadyEvent) {
 
             TextChannel channel = _bot.getTextChannelById(_channelID);
-            channel.sendMessage("Bot starting up").queue();
+            if(channel != null && _showStatusMessages)
+                channel.sendMessage("MCord online \\o").queue();
             _botReady = true;
         }
         if(event instanceof MessageReceivedEvent){
             MessageReceivedEvent evt = (MessageReceivedEvent)event;
             if(!evt.getAuthor().isBot() && evt.getChannel().getIdLong() == _channelID){
-                onDiscordMessage(evt.getMember().getEffectiveName(), evt.getMessage().getContentDisplay());
+                TextChannel channel = evt.getTextChannel();
+                Member member  = evt.getMember();
+                String message = evt.getMessage().getContentStripped();
+                if(message.startsWith("/online")){
+                    Collection<? extends Player> players =  Bukkit.getServer().getOnlinePlayers();
+                    ArrayList<String> messages = new ArrayList<>();
+                    messages.add("**Currently online:**");
+                    for(Player player : players){
+                        messages.add(player.getDisplayName());
+                    }
+                    if(players.size() == 0){
+                        messages.add("*crickets...*");
+                    }
+                    channel.sendMessage(String.join("\n", messages)).queue();
+                }else if(message.startsWith("/help")){
+                    ArrayList<String> messages = new ArrayList<>();
+                    messages.add("**Commands:**");
+                    messages.add("/help - shows this help message");
+                    messages.add("/online - shows who is online on the server");
+
+                    channel.sendMessage(String.join("\n", messages)).queue();
+                }else{
+                    onDiscordMessage( (member!=null)?member.getEffectiveName():"Unknown User", message);
+                }
             }
         }
     }
--- src/main/java/com/terminal29/MCord.java
+++ src/main/java/com/terminal29/MCord.java
@@ -1,49 +1,94 @@
 package com.terminal29;
 
 import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 
-public final class MCord extends JavaPlugin implements Listener {
+public final class MCord extends JavaPlugin{
 
-    static boolean initSuccess = false;
+    static boolean initSuccess = true;
 
-    @Override
-    public void onEnable() {
-        // Plugin startup logic
+    private boolean isBotEnabled(){
+        return this.getConfig().isSet("enabled") && this.getConfig().getBoolean("enabled");
+    }
+
+    private boolean doPostStatusMessages(){
+        return this.getConfig().isSet("postStatusMessages") && this.getConfig().getBoolean("postStatusMessages");
+    }
+
+    @Nullable
+    private String getBotToken(){
+        if(!this.getConfig().isSet("token") || this.getConfig().getString("token") == null || this.getConfig().getString("token").length() == 0){
+            return null;
+        }
+        return this.getConfig().getString("token");
+    }
+
+    @Nullable Long getBotChannel(){
+        if(!this.getConfig().isSet("channelID") || this.getConfig().getLong("channelID") == 0){
+            return null;
+        }
+        return this.getConfig().getLong("channelID");
+    }
+
+    private boolean startBot() throws Exception {
+        if (!isBotEnabled()) {
+            return false;
+        }
         getLogger().info("MCord starting up");
 
-        if(!this.getConfig().isSet("token") || this.getConfig().getString("token") == null){
-            getLogger().log(Level.SEVERE, "Cannot find bot token string. Add 'token' field to config file");
-            this.saveDefaultConfig();
+        String botToken = getBotToken();
+        Long botChannelID = getBotChannel();
+        boolean doPostStatusMessages = doPostStatusMessages();
+
+        if (botToken == null) {
+            getLogger().log(Level.SEVERE, "Cannot find valid bot token string. Please set 'token' field in config file");
             initSuccess = false;
-        }if(!this.getConfig().isSet("channelID")){
-            getLogger().log(Level.SEVERE, "Cannot find channelID number. Add 'channelID' field to config file");
-            this.saveDefaultConfig();
+        }
+        if (botChannelID == null) {
+            getLogger().log(Level.SEVERE, "Cannot find valid bot channelID number. Please set 'channelID' field in config file");
             initSuccess = false;
-        }else{
+        }
+
+
+        if (initSuccess) {
+            DiscordBot.initBot(botToken, botChannelID, doPostStatusMessages);
             initSuccess = true;
         }
 
-        if(initSuccess){
-            try{
-                DiscordBot.setToken(this.getConfig().getString("token"));
-                DiscordBot.setChannel(this.getConfig().getLong("channelID"));
-                DiscordBot.initBot();
-                initSuccess = true;
-            }catch(Exception e){
-                getLogger().log(Level.SEVERE, "Bot failed to init");
-                getLogger().log(Level.SEVERE, e.getMessage());
-                initSuccess = false;
-            }
+        return initSuccess;
+    }
+
+    private void stopBot(){
+        if(!isBotEnabled()){
+            return;
+        }
+        DiscordBot.cleanupBot();
+    }
+
+    @Override
+    public void onEnable() {
+        boolean result = false;
+        try {
+            result = startBot();
+        } catch (Exception e){
+            getLogger().log(Level.SEVERE, e.getMessage());
         }
 
-        if(initSuccess){
-            getLogger().info("Bot is now online");
+        if(result){
             getServer().getPluginManager().registerEvents(new Listener() {
                 @EventHandler
                 public void onChat(AsyncPlayerChatEvent ev) {
@@ -54,12 +99,87 @@ public final class MCord extends JavaPlugin implements Listener {
                 }
             }, this);
         }
+
+
+        this.getCommand("mcord").setTabCompleter((sender, command, alias, args) -> {
+            List<String> list = new ArrayList<>();
+            if(args.length == 1){
+                list.add("reload");
+                list.add("set");
+            }else if(args.length == 2){
+                if(args[1].equalsIgnoreCase("set")){
+                    list.add("token");
+                    list.add("channelID");
+                }
+            }else if(args.length == 3){
+                if(args[2].equalsIgnoreCase("token")){
+                    list.add("<token>");
+                }else if(args[2].equalsIgnoreCase("channelID")){
+                    list.add("<channelID>");
+                }
+            }
+            return list;
+        });
+        this.getCommand("mcord").setExecutor((sender, command, label, args) -> {
+            if(args.length > 0){
+                if(args.length == 1 && args[0].equalsIgnoreCase("reload") && sender.hasPermission("mcord.reload")) {
+                    sender.sendMessage("Reloading MCord...");
+                    stopBot();
+                    try {
+                        boolean restartResult = startBot();
+                        if(restartResult) {
+                            sender.sendMessage("Reload complete");
+                        }else{
+                            sender.sendMessage("Failed to reload");
+                            sender.sendMessage("See console for more info");
+                        }
+                    }catch(Exception e){
+                        sender.sendMessage("Failed to reload");
+                        sender.sendMessage(e.getMessage());
+                    }
+                }else if(args[0].equalsIgnoreCase("set") && sender.hasPermission("mcord.set")){
+                    if(args.length == 3){
+                        if(args[1].equalsIgnoreCase("token")){
+                            this.getConfig().set("token", args[2]);
+                            sender.sendMessage("Updated bot token");
+                            sender.sendMessage("To apply changes, run /mcord reload");
+                        }if(args[1].equalsIgnoreCase("channelID")){
+                            try {
+                                this.getConfig().set("channelID", Long.parseLong(args[2]));
+                                sender.sendMessage("Updated bot channel id");
+                                sender.sendMessage("To apply changes, run /mcord reload");
+                            }catch(Exception e){
+                                sender.sendMessage("Invalid channel id");
+                                sender.sendMessage(e.getMessage());
+                            }
+
+                        }else{
+                            sender.sendMessage("Invalid command");
+                            sender.sendMessage("Please refer to the command list by running /mcord");
+                        }
+                    }else{
+                        sender.sendMessage("Invalid command");
+                        sender.sendMessage("Please refer to the command list by running /mcord");
+                    }
+                }
+                else{
+                    sender.sendMessage("You don't have permission to run this command");
+                }
+            }else{
+                sender.sendMessage("/mcord - Shows this list of available commands");
+                sender.sendMessage("/mcord set token <token> - Sets the token string the bot will use to connect (dont use quotes)");
+                sender.sendMessage("/mcord set channelID <channelID> - Sets the channel is number the bot will use to connect");
+                sender.sendMessage("/mcord reload - Reloads MCord");
+            }
+            return true;
+        });
     }
+
 
     @Override
     public void onDisable() {
-        DiscordBot.cleanupBot();
+        stopBot();
+        this.saveConfig();
     }
-
 
 }
